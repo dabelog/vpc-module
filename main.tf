7eb613696516c2d9ed7abd1bc669c0f482e109e9
dabelog <151689445+dabelog@users.noreply.github.com>
2023-11-21 21:29:31 -0600

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,58 +1,96 @@
 resource "aws_vpc" "this" {
-  count = var.create_vpc ? 1 : 0
+  count = var.create_vpc ? 1 : 0
 
-  cidr_block                           = var.cidr_block
-  instance_tenancy                     = var.instance_tenancy
-  ipv4_ipam_pool_id                    = var.ipv4_ipam_pool_id
-  ipv4_netmask_length                  = var.ipv4_netmask_length
-  ipv6_cidr_block                      = var.ipv6_cidr_block
-  ipv6_ipam_pool_id                    = var.ipv6_ipam_pool_id
-  ipv6_netmask_length                  = var.ipv6_netmask_length
-  ipv6_cidr_block_network_border_group = var.ipv6_cidr_block_network_border_group
-  enable_dns_support                   = var.enable_dns_support
-  enable_network_address_usage_metrics = var.enable_network_address_usage_metrics
-  enable_dns_hostnames                 = var.enable_dns_hostnames
+  cidr_block                           = var.cidr_block
+  instance_tenancy                     = var.instance_tenancy
+  ipv4_ipam_pool_id                    = var.ipv4_ipam_pool_id
+  ipv4_netmask_length                  = var.ipv4_netmask_length
+  ipv6_cidr_block                      = var.ipv6_cidr_block
+  ipv6_ipam_pool_id                    = var.ipv6_ipam_pool_id
+  ipv6_netmask_length                  = var.ipv6_netmask_length
+  ipv6_cidr_block_network_border_group = var.ipv6_cidr_block_network_border_group
+  enable_dns_support                   = var.enable_dns_support
+  enable_network_address_usage_metrics = var.enable_network_address_usage_metrics
+  enable_dns_hostnames                 = var.enable_dns_hostnames
 
-  enable_classiclink               = var.enable_classiclink
-  enable_classiclink_dns_support   = var.enable_classiclink_dns_support
-  assign_generated_ipv6_cidr_block = var.assign_generated_ipv6_cidr_block
+  enable_classiclink               = var.enable_classiclink
+  enable_classiclink_dns_support   = var.enable_classiclink_dns_support
+  assign_generated_ipv6_cidr_block = var.assign_generated_ipv6_cidr_block
 
 
-  tags = var.tags
-  lifecycle {
-    create_before_destroy = true
-  }
+  tags = var.tags
+  lifecycle {
+    create_before_destroy = true
+  }
 }
 
 #### DHCP Options ####
 
 resource "aws_vpc_dhcp_options" "this" {
-  count = var.create_vpc ? 1 : 0
+  count = var.create_vpc ? 1 : 0
 
-  domain_name          = var.domain_name
-  domain_name_servers  = var.domain_name_servers
-  ntp_servers          = var.ntp_servers
-  netbios_name_servers = var.netbios_name_servers
-  netbios_node_type    = var.netbios_node_type
+  domain_name          = var.domain_name
+  domain_name_servers  = var.domain_name_servers
+  ntp_servers          = var.ntp_servers
+  netbios_name_servers = var.netbios_name_servers
+  netbios_node_type    = var.netbios_node_type
 
-  tags = var.tags
+  tags = var.tags
 
-  lifecycle {
-    create_before_destroy = true
-  }
+  lifecycle {
+    create_before_destroy = true
+  }
 }
 
-### Ensure the default security group of every VPC restricts all traffic  and NACL
+### Ensure the default security group of every VPC restricts all traffic  and NACL
 
 resource "aws_default_security_group" "this" {
-  count = var.create_vpc ? 1 : 0
+  count = var.create_vpc ? 1 : 0
 
-  vpc_id = aws_vpc.this[0].id
-  tags   = var.tags
+  vpc_id = aws_vpc.this[0].id
+  tags   = var.tags
 }
 
-resource "aws_default_network_acl" "this" {
-  default_network_acl_id = aws_vpc.this[0].default_network_acl_id
+resource "aws_security_group" "allow_tls" {
+  name        = "allow_tls"
+  description = "Allow TLS inbound traffic"
+  vpc_id      = ws_vpc.this.id
 
-  tags = var.tags
+  ingress {
+    description      = "TLS from VPC"
+    from_port        = 443
+    to_port          = 443
+    protocol         = "tcp"
+    cidr_blocks      = [ws_vpc.this.cidr_block]
+  }
+
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
+  tags = {
+    Name = "allow_tls"
+  }
+}
+
+resource "aws_subnet" "public_subnet" {
+  vpc_id     = aaws_vpc.this.id
+  cidr_block = "10.0.0.0/20"
+
+  tags = {
+    Name = "public"
+  }
+}
+
+resource "aws_subnet" "private_subnet" {
+  vpc_id     = aws_vpc.main.id
+  cidr_block = "10.0.16.0/20"
+
+  tags = {
+    Name = "private"
+  }
 }
